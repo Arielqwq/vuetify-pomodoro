9a59282ba6b008af74830a2a7bc0dc9a7dcf94c9
Ariel <dawei4102@gmail.com>
2022-12-26 19:06:40 +0800

    1226

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -1,31 +1,31 @@
-import { createRouter, createWebHashHistory } from "vue-router";
-import HomeView from "@/views/HomeView.vue";
+import { createRouter, createWebHashHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.BASE_URL),
   routes: [
     {
-      path: "/",
-      name: "home",
+      path: '/',
+      name: 'home',
       component: HomeView,
       meta: {
-        title: "番茄鐘 ",
-      },
+        title: '番茄鐘 '
+      }
     },
     {
-      path: "/settings",
-      name: "settings",
-      component: () => import("@/views/SettingsView.vue"),
+      path: '/settings',
+      name: 'settings',
+      component: () => import('@/views/SettingsView.vue'),
       meta: {
-        title: "番茄鐘 | 清單",
-      },
+        title: '番茄鐘 | 清單'
+      }
     },
     {
-      path: "/list",
-      name: "list",
-      component: () => import("@/views/ListView.vue"),
-    },
-  ],
-});
+      path: '/list',
+      name: 'list',
+      component: () => import('@/views/ListView.vue')
+    }
+  ]
+})
 
-export default router;
+export default router
